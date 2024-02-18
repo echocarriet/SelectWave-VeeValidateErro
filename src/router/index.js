35e3f88dc259cbc2280d44c7f382f8649818f591
carrieTseng <echocarriet@gmail.com>
2024-02-18 09:53:16 +0800

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,53 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/WebLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'HomeDefault',
+        component: () => import('../views/HomeView.vue'),
+      },
+      {
+        path: '/about',
+        name: 'About',
+        component: () => import('../views/AboutView.vue'),
+      },
+      {
+        path: '/privacy',
+        name: 'Privacy',
+        component: () => import('../views/PrivacyView.vue'),
+      },
+      {
+        path: '/terms',
+        name: 'Terms',
+        component: () => import('../views/TermsView.vue'),
+      },
+      {
+        path: '/form',
+        name: 'Form',
+        component: () => import('../views/FormView.vue'),
+      },
+    ],
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('../views/NotFoundView.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/404',
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  linkActiveClass: 'active',
+  routes,
+});
+
+export default router;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div style="height:200vh" class="max-w-screen-lg mx-auto">eujowo</div>
+</template>
